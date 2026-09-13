f9a624e4f803d2d07d8f3163d3bc9ff22e97ff60
Aman Khan <amanx11n@gmail.com>
2026-09-13 15:52:27 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.25%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+//     bool solve(vector<int>& nums, int i){
+//         // base case :-
+//         if(i == nums.size() - 1) return true;
+//         if(i>= nums.size()) return false; 
+//         if(nums[i]==0) return false; // stuck at the index handles the case in which jump is not possible.
+
+//         bool recans= false;
+//         for(int jump = 1; jump <= nums[i]; jump++){
+//             // recursive call
+//             recans = recans || solve(nums,i + jump);
+//         }
+//         return recans;
+//     }
+//     bool canJump(vector<int>& nums) {
+//         int i = 0;
+//         return solve(nums, i);
+//     }
+// };
+
+class Solution {
+public: // Greedy approach :
+    bool canJump(vector<int>& nums) {
+        int max_index = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(i > max_index){
+                return false;
+            }
+            max_index = max(max_index, i + nums[i]);
+        }
+        return true;
+    }
+};
+
